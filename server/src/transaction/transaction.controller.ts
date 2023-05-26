a3ee2d7653e7bbe1b85d898812de13e1af6fa12a
carraes <carraeshb@gmail.com>
2023-05-26 11:20:34 -0300

    feat: create expense/revenue routing

--- server/src/transaction/transaction.controller.ts
+++ server/src/transaction/transaction.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { GetUser } from 'src/auth/decorator'
 import { JwtGuard } from 'src/auth/guard'
+import { ExpenseDto, RevenueDto } from './dto'
 import { TransactionService } from './transaction.service'
 
+@UseGuards(JwtGuard)
 @Controller('transaction')
 export class TransactionController {
   constructor(private transactionService: TransactionService) {}
 
-  @UseGuards(JwtGuard)
-  @Post()
-  createTransaction() {
-    return { message: 'Transaction created' }
+  @Post('expense')
+  async createExpense(@Body() dto: ExpenseDto, @GetUser('id') userId: number) {
+    return await this.transactionService.createExpense(userId, dto)
+  }
+
+  @Post('revenue')
+  async createRevenue(@Body() dto: RevenueDto, @GetUser('id') userId: number) {
+    return await this.transactionService.createRevenue(userId, dto)
   }
 }
--- server/src/transaction/transaction.service.ts
+++ server/src/transaction/transaction.service.ts
@@ -1,4 +1,81 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { ExpenseDto, RevenueDto } from './dto'
 
 @Injectable()
-export class TransactionService {}
+export class TransactionService {
+  constructor(private prisma: PrismaService) {}
+
+  async createExpense(userId: number, dto: ExpenseDto) {
+    await this.verifyExists('category', dto.categoryId)
+    await this.verifyExists('company', dto.companyId)
+    await this.verifyExists('user', userId)
+
+    const transaction = this.prisma.expense.create({
+      data: {
+        description: dto.description,
+        amount: dto.amount,
+        transactionDate: dto.transactionDate,
+        recurring: dto.recurring,
+        recurringInterval: dto.recurringInterval,
+        category: {
+          connect: {
+            id: dto.categoryId,
+          },
+        },
+        company: {
+          connect: {
+            id: dto.companyId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    })
+
+    return transaction
+  }
+
+  async createRevenue(userId: number, dto: RevenueDto) {
+    await this.verifyExists('company', dto.companyId)
+    await this.verifyExists('user', userId)
+
+    const transaction = this.prisma.revenue.create({
+      data: {
+        transactionDate: dto.transactionDate,
+        cash: dto.cash,
+        pix: dto.pix,
+        credit: dto.credit,
+        debit: dto.debit,
+        meal: dto.meal,
+        total: dto.cash + dto.pix + dto.credit + dto.debit + dto.meal,
+        company: {
+          connect: {
+            id: dto.companyId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    })
+
+    return transaction
+  }
+
+  private async verifyExists(model: string, id: number) {
+    const exists = await this.prisma[model].findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!exists)
+      throw new HttpException(`${model} not found`, HttpStatus.NOT_FOUND)
+  }
+}
