d27401c98bd610f7ee21ef1474027f1064aded5c
carraes <carraeshb@gmail.com>
2023-05-26 16:36:18 -0300

    feat: setup login component with route

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-import Hello from './components/Hello.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-4xl">Hello</h1>
-  <Hello />
+  <router-view />
 </template>
 
 <style scoped></style>
---
+++ client/src/pages/Login.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+console.log('hello')
+</script>
+
+<template>
+  <h1 class="text-4xl">Login</h1>
+</template>
