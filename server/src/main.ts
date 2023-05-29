0c0d3cd71515d52fe839d31935d36232fdc949b7
carraes <carraeshb@gmail.com>
2023-05-29 17:09:18 -0300

    feat: setup morgan for logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as morgan from 'morgan'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -10,6 +11,7 @@ async function bootstrap() {
     }),
   )
   app.enableCors()
+  app.use(morgan('dev'))
   await app.listen(8080)
 }
 bootstrap()
