22423f3fa3c09577c25ee22b5755e0aebbb7a356
carraes <carraeshb@gmail.com>
2023-05-29 17:10:31 -0300

    feat: Relatorio page, still needs work

---
+++ client/src/pages/Relatorio.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Nav from '../components/Nav.vue'
+</script>
+
+<template>
+  <Nav />
+  <main>
+    <h1>Relatorio</h1>
+  </main>
+</template>
+
+<style scoped>
+nav {
+  height: 15vh;
+}
+
+main {
+  height: 85vh;
+}
+</style>
